d706f2591b739193c21e7b4b7bf6e5ed91378427
Fan S <sfan@m-wise.nl>
2019-02-26 21:28:06 +0100

    CommonChild not fully correct

--- Models/CommonChild.cs
+++ Models/CommonChild.cs
@@ -12,10 +12,16 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
+// class Data{
+//     public char c;
+//     public int i;
+// }
+
 class CommonChild {
 
     // Complete the commonChild function below.
     static int commonChild(string s1, string s2) {
+        /* 
         int result = 0;
         int pA = 0;
         int pB = 0;
@@ -58,21 +64,72 @@ class CommonChild {
         }
 
         return result;
+
+        */
+
+        var results = new List<int>();
+        var maxLen = 0;
+
+        foreach(var c in s1)
+        {
+            var index = s2.IndexOf(c);
+
+            while(results.Contains(index))
+            {
+                index = s2.IndexOf(c, index + 1);
+            }
+
+            if(index >= 0)
+            {
+                Console.WriteLine(c + "-" + index);
+                var j=0;
+                for(; j<results.Count; j++)
+                {
+                    if(results[j] >= index)
+                    {
+                        results[j] = index;
+                        break;
+                    }
+                }
+
+                if(j == results.Count)
+                {
+                    results.Add(index);
+                }
+            }
+        }
+
+
+        return maxLen = results.Count;
     }
 
     static void Main(string[] args) {
 
-        string s1 = "SHINCHAN";
-        string s2 = "NOHARAAA";
+        string s1 = "SHINCHANHA";
+        string s2 = "NOHARAAAHA";
 
         // int a = "FG".IndexOf('G');
 
         int result = commonChild(s1, s2);
         Console.WriteLine(result);
 
+        s1 = "HARRY";
+        s2 = "SALLY";
+        result = commonChild(s1, s2);
+        Console.WriteLine(result);
+
+        s1 = "ABCDEF";
+        s2 = "FBDAMN";
+        result = commonChild(s1, s2);
+        Console.WriteLine(result);
+
+        s1 = "SHINCHAN";
+        s2 = "NOHARAAA";
+        result = commonChild(s1, s2);
+        Console.WriteLine(result);
+
         s1 = "WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS";
         s2 = "FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC";
-
         result = commonChild(s1, s2);
         Console.WriteLine(result);
 
