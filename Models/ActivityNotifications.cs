82fb5dced60bd192e8f11a705596b120816f197d
Fan S <sfan@m-wise.nl>
2019-03-02 17:35:10 +0100

    updated ActivityNotifications using insertion sort still has performance issue...

--- Models/ActivityNotifications.cs
+++ Models/ActivityNotifications.cs
@@ -32,43 +32,71 @@ class ActivityNotifications {
 
     // Complete the activityNotifications function below.
     static int activityNotifications(int[] expenditure, int d) {
-        var exp = new List<int>(expenditure);
-        var n = exp.Count();
+        // var exp = new List<int>(expenditure);
+        var n = expenditure.Length;
         var count = 0;
 
-        if(n <= d)
+        if(expenditure.Length <= d)
         {
             return 0;
         }
 
+        var isOdd = d % 2 == 1 ? true : false;
+        int index = d / 2;
+        var arr = expenditure.Take(d).ToList();
+        arr.Sort();
+
         for(var i = d; i < n; i++)
         {
-            var tmp = exp.Skip(i - d).Take(d).ToList();
-            if(exp[i] >= median_2(tmp))
+            //var tmp = exp.Skip(i - d).Take(d).ToList();
+            var median = 0;
+            if(isOdd)
+            {
+                median =  2 * arr[index];
+            }
+            else
+            {
+                median =  arr[index - 1] + arr[index];
+            }
+
+            if(expenditure[i] >= median)
             {
                 count++;
             }
+
+
+            // prepare for next round
+            // remove first value
+            var toBeRemovedIndex = arr.IndexOf(expenditure[i - d]);
+            arr.RemoveAt(toBeRemovedIndex);
+
+            // add new value
+            var toBeAdded = expenditure[i];
+            // since one element has been removed
+            var pos = d - 1;
+            arr.Add(toBeAdded);
+            while(pos > 0 && toBeAdded < arr[pos - 1])
+            {
+                arr[pos] = arr[pos - 1];
+                pos -= 1;
+            }
+
+            arr[pos] = toBeAdded;
         }
 
         return count;
     }
 
     static void Main(string[] args) {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        string[] nd = Console.ReadLine().Split(' ');
 
-        int n = Convert.ToInt32(nd[0]);
-
-        int d = Convert.ToInt32(nd[1]);
+        int d = Convert.ToInt32(5);
 
         int[] expenditure = Array.ConvertAll(Console.ReadLine().Split(' '), expenditureTemp => Convert.ToInt32(expenditureTemp))
         ;
         int result = activityNotifications(expenditure, d);
 
-        textWriter.WriteLine(result);
+        Console.WriteLine(result);
 
-        textWriter.Flush();
-        textWriter.Close();
     }
 }
--- Models/CountTriplets.cs
+++ Models/CountTriplets.cs
@@ -71,21 +71,21 @@ class CountTriplets {
         return result;
     }
 
-    static void Main(string[] args) {
+    // static void Main(string[] args) {
 
-        // long r = Convert.ToInt64(1);
-        long r = Convert.ToInt64(3);
+    //     // long r = Convert.ToInt64(1);
+    //     long r = Convert.ToInt64(3);
 
-        // List<long> arr = "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1".TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
-        // List<long> arr = "1 2 2 4".TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
-        // List<long> arr = "1 3 9 9 27 81".TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
-        // List<long> arr = "1237 1237 1237 1237 1237 1237 1237 1237 1237 1237 1237 1237 1237 1237".TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
-        // List<long> arr = "1 17 80 68 5 5 58 17 38 81 26 44 38 6 12 11 37 67 70 16 19 35 71 16 32 45 7 39 2 14 16 78 82 5 18 86 61 37 12 8 27 90 13 26 57 24 36 4 52 67 71 71 11 51 48 42 57 16 43 58 29 58 8 20 24 25 15 84 61 78 53 49 39 66 75 6 51 72 9 13 49 79 45 21 1 2 63 20 17 67 39 45 86 46 26 19 70 2 64 2 79 78 51 28 53 87 85 14 68 55 78 78 5 32 9 57 85 71 76 11 9 25 17 4 32 42 74 64 5 47 65 83 34 77 72 49 73 66 24 13 82 11 90 86 4 8 53 88 40 38 8 48 24 76 13 56 79 86 29 83 4 3 37 38 42 19 48 24 46 71 36 38 81 88 85 46 57 47 43 7 85 50 16 70 87".TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
-        List<long> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
+    //     // List<long> arr = "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1".TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
+    //     // List<long> arr = "1 2 2 4".TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
+    //     // List<long> arr = "1 3 9 9 27 81".TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
+    //     // List<long> arr = "1237 1237 1237 1237 1237 1237 1237 1237 1237 1237 1237 1237 1237 1237".TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
+    //     // List<long> arr = "1 17 80 68 5 5 58 17 38 81 26 44 38 6 12 11 37 67 70 16 19 35 71 16 32 45 7 39 2 14 16 78 82 5 18 86 61 37 12 8 27 90 13 26 57 24 36 4 52 67 71 71 11 51 48 42 57 16 43 58 29 58 8 20 24 25 15 84 61 78 53 49 39 66 75 6 51 72 9 13 49 79 45 21 1 2 63 20 17 67 39 45 86 46 26 19 70 2 64 2 79 78 51 28 53 87 85 14 68 55 78 78 5 32 9 57 85 71 76 11 9 25 17 4 32 42 74 64 5 47 65 83 34 77 72 49 73 66 24 13 82 11 90 86 4 8 53 88 40 38 8 48 24 76 13 56 79 86 29 83 4 3 37 38 42 19 48 24 46 71 36 38 81 88 85 46 57 47 43 7 85 50 16 70 87".TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
+    //     List<long> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
 
-        long ans = countTriplets(arr, r);
+    //     long ans = countTriplets(arr, r);
 
-        Console.WriteLine(ans);
+    //     Console.WriteLine(ans);
 
-    }
+    // }
 }
