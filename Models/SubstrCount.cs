b13712b9907c9674a7da8b10b73f75fc1db1a2cc
Fan S <sfan@m-wise.nl>
2019-03-03 08:40:59 +0100

    SubstrCount final

--- Models/SubstrCount.cs
+++ Models/SubstrCount.cs
@@ -13,57 +13,105 @@ using System.Text;
 using System;
 
 class SubstrCount {
-    static bool isPalindromic (int n, string s)
-    {
-        if( n % 2 == 1)
-        {
-            int mid = n / 2;
+    // static bool isPalindromic (int n, string s)
+    // {
+    //     if( n % 2 == 1)
+    //     {
+    //         int mid = n / 2;
 
-            for(int i = 0; i < n - 1; i++)
-            {
-                if(i + 1 == mid || i == mid)
-                {
-                    if(s[mid - 1] != s[mid + 1])
-                    {
-                        return false;
-                    }
-                }
-                else
-                {
-                    if(s[i] != s[i+1])
-                    {
-                        return false;
-                    }
-                }
-            }
-        }
-        else
-        {
-            for(int i = 0; i < n - 1; i++)
-            {
-                if(s[i] != s[i+1])
-                {
-                    return false;
-                }
-            }
-        }
+    //         for(int i = 0; i < n - 1; i++)
+    //         {
+    //             if(i + 1 == mid || i == mid)
+    //             {
+    //                 if(s[mid - 1] != s[mid + 1])
+    //                 {
+    //                     return false;
+    //                 }
+    //             }
+    //             else
+    //             {
+    //                 if(s[i] != s[i+1])
+    //                 {
+    //                     return false;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     else
+    //     {
+    //         for(int i = 0; i < n - 1; i++)
+    //         {
+    //             if(s[i] != s[i+1])
+    //             {
+    //                 return false;
+    //             }
+    //         }
+    //     }
 
-        return true;
-    }
+    //     return true;
+    // }
 
     // Complete the substrCount function below.
     static long substrCount(int n, string s) {
         long result = 0;
         result += n;
 
-        for (var i = 2; i <= n; i++)
+        // for (var i = 2; i <= n; i++)
+        // {
+        //     for(var j = 0; j <= n - i; j++)
+        //     {
+        //         var p = isPalindromic(i, s.Substring(j, i));
+        //         if(p)
+        //         {
+        //             result++;
+        //         }
+        //     }
+        // }
+
+        // reuse the result of previous round.
+        /*
+        for example: asassdss
+        as, asa count++
+        sa, sas count++
+        as, asd 
+        ss, ssd, ssds, ssdss, count + 2
+        sd, sds count++
+        ds, dss
+        ss  count++
+
+         */
+        for (var i = 0; i < n; i++)
         {
-            for(var j = 0; j <= n - i; j++)
+            var sameCount = 1;
+            var mid = false;
+            for(var j = i + 1; j < n; j++)
             {
-                var p = isPalindromic(i, s.Substring(j, i));
-                if(p)
+                if(s[i] == s[j])
                 {
-                    result++;
+                    if(mid)
+                    {
+                        if(j - i == 2 * sameCount)
+                        {
+                            result++;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        sameCount++;
+                        result++;
+                    }
+                }
+                else
+                {
+                    if(!mid)
+                    {
+                        mid = true;
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
             }
         }
@@ -73,10 +121,10 @@ class SubstrCount {
     }
 
     static void Main(string[] args) {
-        int n = 5;
+        int n = 8;
 
         // string s = Console.ReadLine();
-        string s = "asasd";
+        string s = "asassdss";
 
         long result = substrCount(n, s);
 
